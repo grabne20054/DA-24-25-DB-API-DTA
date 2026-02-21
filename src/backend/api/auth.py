92ab38749eb88e931242e190b26eb6f51797574f
grabne20054 <grabne20054@gmail.com>
2026-02-21 12:07:25 +0100

    refactor: simplify token validation logic and remove JWT decoding

--- src/backend/api/auth.py
+++ src/backend/api/auth.py
@@ -7,15 +7,14 @@ load_dotenv()
 
 ALGORITHM = "HS256"
 
-def decode_jwt_token(token: str, verify_expiration: bool):
-    decoded_token = jwt.decode(token, getenv("JWT_SECRET_KEY"), algorithms=[ALGORITHM], options={"verify_exp": verify_expiration})
-    return decoded_token
-
 def is_token_valid(token: str):
-    try:
-        decode_jwt_token(token, verify_expiration=True)
-    except jwt.ExpiredSignatureError:
-        raise HTTPException(status_code=401, detail="Token has expired")
-    except jwt.InvalidTokenError:
-        raise HTTPException(status_code=401, detail="Invalid token")
-    return True
+    if check_token(token):
+        return True
+    else:
+        raise HTTPException(status_code=401, detail="Invalid API key")
+
+def check_token(token: str):
+    if token == getenv("API_KEY"):
+        return True
+    else:
+        return False
